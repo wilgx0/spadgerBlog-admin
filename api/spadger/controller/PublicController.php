eb8e57b265a1d374e889922ae3d98fe980846793
wilgx0 <396861557@qq.com>
2018-08-20 11:46:28 +0800

    first commit

---
+++ api/spadger/controller/PublicController.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Dean <zxxjjforever@163.com>
+// +----------------------------------------------------------------------
+namespace api\spadger\controller;
+
+use cmf\controller\RestBaseController;
+use think\Db;
+use think\Validate;
+
+class PublicController extends RestBaseController
+{
+
+    public function test(){
+        echo 'test';
+    }
+
+}
