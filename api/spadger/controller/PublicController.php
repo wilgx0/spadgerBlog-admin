71c3014f8e8d81344cebcf264003e0a0bc78b3d0
wilgx0 <396861557@qq.com>
2018-08-21 14:42:31 +0800

    11

--- api/spadger/controller/PublicController.php
+++ api/spadger/controller/PublicController.php
@@ -11,6 +11,7 @@ namespace api\spadger\controller;
 use cmf\controller\RestBaseController;
 use think\Db;
 use think\Validate;
+use api\portal\model\PortalPostModel;
 
 class PublicController extends RestBaseController
 {
@@ -19,4 +20,23 @@ class PublicController extends RestBaseController
         echo 'test';
     }
 
+    /**
+     *
+     */
+    public function get_share_list(){
+        $PortalPostModel = new PortalPostModel();
+        $where = [];
+        $where['post_type'] = 1;        //文章类型:1 文章 2 页面
+        $where['post_status'] = 1;      //状态： 1 已发布  2 未发布
+        $list = $PortalPostModel
+            ->order('id desc')
+            ->where($where)
+            ->field('id,post_title,post_excerpt')
+            ->paginate(15);
+        if($list->isEmpty()){
+            $this->error('没有可以显示的数据!');
+        } else {
+            $this->success('请求成功', $list);
+        }
+    }
 }
